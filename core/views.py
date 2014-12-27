16b473aa5ccba5ea6b625e74272a8cbd44bb2d90
olunx <olunx@qq.com>
2014-12-28 00:36:56 +0800

    Initial

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render_to_response
+
+# Create your views here.
+
+
+def home(request):
+    return render_to_response('index.html')
+
+
+def hot_stuff(request):
+    return render_to_response('hotstuff.html')
