5707e1406aa27caf7f42087b048731b5ac0333fb
olunx <olunx@qq.com>
2015-01-01 01:54:50 +0800

    阿里巴巴单页抓取功能

--- apps/alibabachina/models.py
+++ apps/alibabachina/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'olunx'
 
+import ast
 from django.db import models
 
 
@@ -16,6 +17,12 @@ class AlibabachinaScrapy(models.Model):
     sold = models.IntegerField(null=True, verbose_name='Sold')
     category = models.CharField(max_length=30, null=True, verbose_name='Category')
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='Create Time')
+
+    def get_image_list(self):
+        data = []
+        if self.image:
+            data = ast.literal_eval(self.image)
+        return list(set(data))
 
     def __unicode__(self):
         return self.title
